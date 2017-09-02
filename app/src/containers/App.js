6207027f08b18383fbee024a4d5430e2abc91e65
GlyphGryph <glyphgryph@gmail.com>
2017-09-02 16:52:03 -0400

    Keypresses now regenerate grid

--- app/src/containers/App.js
+++ app/src/containers/App.js
@@ -2,8 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import CurrentGrid from '../containers/CurrentGrid'
+import { newGrid } from '../actions'
 
 class App extends React.Component{
+ constructor() {
+    super();
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleKeyPress(event) {
+    this.props.keyPressAction(event.keyCode);
+  }
+
+  componentDidMount() {
+     document.addEventListener('keypress', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+     document.removeEventListener('keypress', this.handleKeyPress);
+  }
 
   render(){
     return (
@@ -14,4 +31,12 @@ class App extends React.Component{
   }
 }
 
-export default connect()(App)
+const mapDispatchToProps = dispatch => {
+  return {
+    keyPressAction: keyCode => {
+      dispatch(newGrid(10,20))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App)
