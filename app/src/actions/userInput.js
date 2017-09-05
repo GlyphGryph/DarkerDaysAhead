22186910f982d5690c239879f9d9dcac91a2acfe
GlyphGryph <glyphgryph@gmail.com>
2017-09-05 08:57:06 -0400

    Spawning in creatures, they randomly spawn in an open edge cell

--- app/src/actions/userInput.js
+++ app/src/actions/userInput.js
@@ -2,16 +2,18 @@ import Keybinds from '../config/keybinds'
 import { resetMap } from './resetMap'
 import { spawnCreature } from './spawnCreature'
 import { move } from './move'
+import helpers from '../logic/helpers'
 
 export const userInput = (key)=> {
   let keyAction = Keybinds.get(key)
   return (dispatch, getState) => {
+    let state = getState()
     if(!keyAction){
       console.log('Key Unbound: '+key)
       return
     } else if(keyAction.value === 'SELECT'){
-      let xx = 0
-      let yy = 0
+      let [xx, yy] = helpers.randomEdgeCoords(state)
+      console.log(''+xx+'/'+yy)
       return dispatch(spawnCreature('KREK', xx, yy))
     }else if(keyAction.value === 'CHANGE') {
       return dispatch(resetMap())
--- app/src/logic/helpers.js
+++ app/src/logic/helpers.js
@@ -1,9 +1,37 @@
-export default {
-  findCellId: (xx, yy, view)=>{
-    // Calculate the id from the coordinates and the width of the grid
-    return yy * view.width + xx
-  },
-  cellIsBlocked: (cell)=>{
-    return cell.contents.length > 0
+const findCellId = (xx, yy, view)=>{
+  // Calculate the id from the coordinates and the width of the grid
+  return yy * view.width + xx
+}
+
+const cellIsBlocked = (cell)=>{
+  return cell.contents.length > 0
+}
+
+const randomEdgeCoords = (state)=>{
+  let edgeCells = [
+    ...state.cells.slice(0, state.view.width),
+    ...state.cells.slice(-state.view.width)
+  ]
+  for(let yy=1; yy < state.view.height-1; yy++){
+    edgeCells.push(
+      state.cells[findCellId(0, yy, state.view)]
+    )
+    edgeCells.push(
+      state.cells[findCellId(state.view.width-1, yy, state.view)]
+    )
   }
+  edgeCells = edgeCells.filter((cell)=>{
+    return cell.contents.length < 1
+  })
+  // If there are no valid cells to spawn in, return an invalid spawn location
+  if(edgeCells.length < 1){
+    return [-1,-1]
+  }
+  let randomIndex = Math.floor(Math.random()*edgeCells.length)
+  return [edgeCells[randomIndex].x, edgeCells[randomIndex].y]
+}
+export default {
+  findCellId,
+  cellIsBlocked,
+  randomEdgeCoords
 }
